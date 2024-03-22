0d2c1199e2a360bf59bab52b64c8349c822d12b3
shellcom <shellcom@qq.com>
2024-03-22 17:05:16 +0800

    中注协审计收入写入

--- makabaka/crawler/gpt-crawler-py/exclehref.py
+++ makabaka/crawler/gpt-crawler-py/exclehref.py
@@ -1,10 +1,11 @@
+import asyncio
 import hashlib
 import json
 import re
-import xlwings as xw
-import pandas as pd
+
 import redis
-from playwright.sync_api import sync_playwright
+import xlwings as xw
+from playwright.async_api import async_playwright
 
 from config import Config
 
@@ -15,6 +16,8 @@ def xlookup(lookup_value, lookup_array, return_array, if_not_found: str = ''):
         return f'"{lookup_value}" 没有找到！' if if_not_found == '' else if_not_found
     else:
         return match_value.tolist()[0]
+
+
 def get_md5(val):
     """把目标数据进行哈希，用哈希值去重更快"""
     md5 = hashlib.md5()
@@ -31,25 +34,26 @@ def add_url(url):
 
 
 # Function to get page HTML
-def get_page_html(page, selector):
-    elements_text = page.locator(selector).all_inner_texts()
-    text = ''
+async def get_page_html(page, selector):
+    await page.wait_for_selector(selector)
+    elements_text = await page.locator(selector).all_inner_texts()
+    text = []
     for element_text in elements_text:
-        text = text + element_text
+        text = text + element_text.split("\t")
     return text
 
 
 # Crawl function
-def crawl(config):
+async def crawl(config):
     results = []
-    queue =ls_data
+    queue = ls_data
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
 
         if config.cookie:
-            page.context.add_cookies([{
+            await page.context.add_cookies([{
                 "name": config.cookie['name'],
                 "value": config.cookie['value'], "url": config.url}])
 
@@ -57,16 +61,26 @@ def crawl(config):
             while queue and len(results) < config.max_pages_to_crawl:
                 url = queue.pop(0)
                 print(f"Crawler: Crawling {url}")
-                page.goto(url)
+                await page.goto(url)
                 if re.search(config.htmlmatch, url):
-                    html = get_page_html(page, config.selector)
-                    results.append({'url': url, 'html': html})
+                    html = await get_page_html(page, config.selector)
+                    results.append(html)
         finally:
-            browser.close()
+            await browser.close()
     return results
+
+
 # Main function
-def main(config):
-    results = crawl(config)
+async def main(config):
+    results = await crawl(config)
+    for j in range(len(results)):
+        sh2(j + 1, 14).value = results[j][1]
+        sh2(j + 1, 15).value = results[j][3]
+        sh2(j + 1, 16).value = results[j][5]
+        sh2(j + 1, 17).value = results[j][7]
+        sh2(j + 1, 18).value = results[j][9]
+        sh2(j + 1, 19).value = results[j][11]
+        sh2(j + 1, 20).value = results[j][13]
     with open(config.output_file_name, 'w', encoding="utf-8") as f:
         json.dump(results, f, ensure_ascii=False, indent=2)
 
@@ -102,8 +116,8 @@ if __name__ == "__main__":
     for i in range(nrows - 1):
         ls_data.append(sh1[i + 1, 9].hyperlink.replace("%23", "#"))
 
-    app.kill()  # 终止进程，强制退出。
+    # app.kill()  # 终止进程，强制退出。
     # app.quit()  # 在不保存的情况下，退出excel程序。
-    main(config)
+    asyncio.run(main(config))
 # ^(tou)((?!zj).)*(?<!jw)$ 包括tou,不包括，不包括jw
 # ^(tou)((?!zj).)*(jw)$ 包括tou,不包括，包括jw
